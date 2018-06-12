81e000643ddf6cf7b6474280f10af20edbfce816
Haodv <haodv@tohsoft.com>
2018-06-12 14:24:58 +0700

    update new code

--- app/src/main/java/com/tohsoft/jenkinandroidex1/MainActivity.java
+++ app/src/main/java/com/tohsoft/jenkinandroidex1/MainActivity.java
@@ -2,6 +2,7 @@ package com.tohsoft.jenkinandroidex1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showToast("Dipng van hao");
+    }
+    private void showToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
